4c70598d9b243fc8c0a2e119d622ecd2a889a14a
Gisele Ribeiro <giselesousar@gmail.com>
2020-11-29 22:53:23 -0300

    #

--- philosophers-problem.c
+++ philosophers-problem.c
@@ -6,19 +6,24 @@
 
 #define N 5 //quantidade de threads
 
-#define LEFT (i+N-1)%N
-#define RIGHT (i+1)%N
+#define LEFT (i+N-1)%N //vizinho esquerdo
+#define RIGHT (i+1)%N //vizinho direito
 
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
 
-pthread_mutex_t mutex;
-pthread_cond_t p[N];
+pthread_mutex_t mutex; //regula o acesso a regiao critica
+pthread_cond_t p[N]; //variaveis de condicao dos filosofos
 
-int state[N];
+int state[N]; //estado atual de cada filosofo {0, 1 , 2} 
 
 
+//parametros: indice referente a uma thread 
+//a funcao verifica se o estado da thread eh 1 e se seus vizinhos nao estao com estado 2
+//retorna 1 caso a condicao seja verdadeira, mudando seu estado para 2 e 
+//sinaliza a thread em questao para desperta-la
+//retorna 0 quando a condicao nao eh satisfeita
 int test(int i){
    
     if(state[i] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING){
@@ -30,6 +35,10 @@ int test(int i){
     return 0;
 }
 
+//parametros: indice referente a uma thread 
+//a funcao muda o estado da thread para 1 e faz uma chamada a funcao test 
+//se test retorna 0, a thread nao mudou seu estado para 2 e 
+//entao eh bloqueada
 void take_forks(int *i){
     pthread_mutex_lock(&mutex);
     state[*i] = HUNGRY;
@@ -37,23 +46,23 @@ void take_forks(int *i){
     int t = test(*i);
 
     if(t == 0){
-        pthread_mutex_unlock(&mutex);
         pthread_cond_wait(&p[*i], &mutex);
     }
     pthread_mutex_unlock(&mutex);
 }
 
+//parametros: indice referente a uma thread
+//a funcao muda o estado da thread para 0 
+//e chama a funcao test passando como parametro as threads vizinhas
 void put_forks(int *i){
     pthread_mutex_lock(&mutex);
     state[*i] = THINKING;
     int t = test((*i+N-1)%N);
-
     t = test((*i+1)%N);
-
     pthread_mutex_unlock(&mutex);
 }
 
-
+//funcao invocada por cada thread para iniciar sua execucao
 void* philosopher(void *context){
 
     int *i = context;
@@ -61,10 +70,11 @@ void* philosopher(void *context){
     while(1){
         printf("Thread %d is thinking! \n", *i);
         sleep(1+ rand()%5); //think
+
         printf("Thread %d is hungry! \n", *i);
         take_forks(i);
-        
         sleep(1+ rand()%8); //eat
+        
         printf("Thread %d put forks! \n", *i);
         put_forks(i);
     }
@@ -74,7 +84,7 @@ void* philosopher(void *context){
 
 int main(){
 
-    pthread_t *philosophers = malloc(sizeof(*philosophers)*5);
+    pthread_t *philosophers = malloc(sizeof(*philosophers)*N);
 
     pthread_mutex_init(&mutex, NULL);
 
